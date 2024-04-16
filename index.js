9df23797f2a7044a961df9feba5e787c69719025
JEAN JOE SILVA DA COSTA <163742297+joejean2002@users.noreply.github.com>
2024-04-16 14:43:14 +0000

    /oi

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+//Exercicio: Criar um endpoint[GET] / oi que exibe: "Olá, mundo!"
+
+app.get('/oi', function(req, res) {
+  res.send('Olá, mundo!')
+})
 
 app.listen(3000)
